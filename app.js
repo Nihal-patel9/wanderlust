13fe9c4a8de825a07a34a9edef506044cede798f
Nihal-patel9 <1999nihalpatel@gmail.com>
2025-05-16 13:34:22 +0530

    Listing model created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,22 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root");
   });
 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
-  });
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute, Goa",
+        country: "India",
+    });
+    
+        await sampleListing.save();
+        console.log("sample was saved");
+        res.send("successful testing");
+   });
+
+
+const port = 5000
+  app.listen(port, () => {
+    console.log(`server running on port : ${port}`)
+})
